23146414ac556e3e48eecf05c33e4c5397257560
honda <clio209@gmail.com>
2021-10-10 21:40:15 +0900

    削除

--- 08.ls_object/lib/ls.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-# frozen_string_literal: true
-
-require_relative './basedata'
-require_relative './command'
-require_relative './output'
-
-output_data = Output.new
-output_data.output
