33ac921651401bc13793182eee7dd3bebf014862
honda <clio209@gmail.com>
2021-10-10 20:51:42 +0900

    rubocop修正

--- 08.ls_object/lib/output.rb
+++ 08.ls_object/lib/output.rb
@@ -4,7 +4,6 @@ require_relative './basedata'
 require_relative './command'
 
 class Output
-
   COLUMN = 3
   FILE_TYPE = { 'file' => '-', 'directory' => 'd', 'link' => 'l' }.freeze
   PERMISSION = { '0' => '---', '1' => '--x', '2' => '-w-', '3' => '-wx', '4' => 'r--', '5' => 'r-x', '6' => 'rw-',
@@ -45,7 +44,7 @@ class Output
       owner = data.nlink.to_s.rjust(4)
       file_size = data.size.to_s.rjust(5)
       last_updated = data.mtime.strftime('%m %d %R')
-      print "#{FILE_TYPE[data.ftype]}#{mode} #{owner} #{Etc.getpwuid(data.uid).name}  #{Etc.getgrgid(data.gid).name}  #{file_size} #{last_updated} #{filename}\n"
+      print "#{FILE_TYPE[data.ftype]}#{mode}#{owner}#{Etc.getpwuid(data.uid).name}#{Etc.getgrgid(data.gid).name}#{file_size}#{last_updated}#{filename}\n"
     end
   end
 
